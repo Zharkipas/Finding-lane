6ac2967b7820104df10cb06f6ce10c4ecc3ac3f5
Zharkipas <zharbiter617@gmail.com>
2022-10-04 16:58:42 +0800

    update

--- lanes.py
+++ lanes.py
@@ -1,5 +1,4 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -9,8 +8,17 @@ def canny(image):
     canny=cv2.Canny(blur,50,150)#change ratio to outline defferent amounts of objects
     return canny
 
+def region_of_interest(image):
+    height=image.shape[0]
+    polygons=np.array([[(200,height),(1100,height),(550,250),]])#coords of triangle (left,right,top)
+    mask=np.zeros_like(image)
+    cv2.fillPoly(mask,polygons,255)
+    masked_image=cv2.bitwise_and(image,mask)
+    return masked_image
+
 image = cv2.imread('test_image.jpg')
 lane_image=np.copy(image)
 canny=canny(lane_image)
-cv2.imshow('result',canny)
+cropped_image=region_of_interest(canny)
+cv2.imshow('result',cropped_image) 
 cv2.waitKey(0)
---
+++ measure.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def canny(image):
+    gray=cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
+    blur=cv2.GaussianBlur(gray,(5,5),0)
+    canny=cv2.Canny(blur,50,150)#change ratio to outline defferent amounts of objects
+    return canny
+
+image = cv2.imread('test.jpg')
+lane_image=np.copy(image)
+canny=canny(lane_image)
+plt.imshow(canny)
+plt.show()
