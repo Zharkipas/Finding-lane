7d9e42e5c89840416785e1bf3ed0f28d05df1270
Zharkipas <zharbiter617@gmail.com>
2022-10-05 14:28:17 +0800

    save

--- measure.py
+++ measure.py
@@ -6,10 +6,10 @@ import numpy as np
 def canny(image):
     gray=cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
     blur=cv2.GaussianBlur(gray,(5,5),0)
-    canny=cv2.Canny(blur,50,150)#change ratio to outline defferent amounts of objects
+    canny=cv2.Canny(blur,100,200)#change ratio to outline defferent amounts of objects
     return canny
 
-image = cv2.imread('test.jpg')
+image = cv2.imread('(720).jpg')
 lane_image=np.copy(image)
 canny=canny(lane_image)
 plt.imshow(canny)
